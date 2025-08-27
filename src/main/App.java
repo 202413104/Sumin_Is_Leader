c973038e6ebad45e0186e508e2eb81533bc4975f
202413104 <suminhyung1227@naver.com>
2025-08-27 17:29:49 +0900

    ì미완성

---
+++ src/main/App.java
@@ -0,0 +1,28 @@
+package main;
+
+
+public class App{
+
+
+
+    static void showMenu(){
+
+        //기능 : 메뉴 구현
+
+    }
+
+
+    static void run(){
+
+        //기능: 프로그램 실행
+    }
+
+
+    public static void main(String[] args){
+
+        showMenu();
+        run();
+
+
+    }
+}
---
+++ src/main/FortuneGame.java
@@ -0,0 +1,30 @@
+package main;
+
+
+import java.util.Map;
+
+
+public class FortuneGame {
+    String userName;
+    Map<Integer, String>options;
+
+
+
+    void start(){
+
+
+    };
+    void inputName(){
+
+    };
+    void chooseOption(int num){
+
+
+    };
+
+//    String getResult(int num){
+//
+//    };
+
+
+}
